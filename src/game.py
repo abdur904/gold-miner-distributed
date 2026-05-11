6cd9b8efa56d7b8f4e684af056371d006c275797
ABDUR <rahim@ABDURs-MacBook-Air.local>
2026-05-11 22:40:49 +1000

    Add empty 20x20 game map

--- src/game.py
+++ src/game.py
@@ -1,3 +1,11 @@
 MAP_SIZE = 20
 
-print("Gold Miner Game Started")
+game_map = []
+
+for i in range(MAP_SIZE):
+    row = []
+    for j in range(MAP_SIZE):
+        row.append(".")
+    game_map.append(row)
+
+print(game_map)
