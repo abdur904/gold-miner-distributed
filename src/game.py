a8bbac6ce20b9fdc8c7fbe8d44090bfe4be1955a
ABDUR <rahim@ABDURs-MacBook-Air.local>
2026-05-11 22:45:24 +1000

    Add game map display function

--- src/game.py
+++ src/game.py
@@ -1,11 +1,24 @@
+# Size of the 2D game map
 MAP_SIZE = 20
 
+# Create empty game map
 game_map = []
 
+# Fill the map with empty cells
 for i in range(MAP_SIZE):
     row = []
+
     for j in range(MAP_SIZE):
         row.append(".")
+
     game_map.append(row)
 
-print(game_map)
+
+# Display the current game map
+def display_map(game_map):
+    for row in game_map:
+        print(" ".join(row))
+
+
+# Show the game map
+display_map(game_map)
